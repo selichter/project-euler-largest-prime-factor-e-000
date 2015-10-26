a8d9912c79bf0c893964dda12e81d7e0a31019df
selichter <lichter.sarahe@gmail.com>
2015-10-26 18:25:15 -0500

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+
+
+require 'prime'
+
+  def largest_prime_factor(num)
+    array = num.prime_division.map(&:first)
+    array.last
+
+  end
+
+
+
+
+ 
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+require 'prime'
+
+class LargestPrimeFactor
+
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def number 
+    array = @num.prime_division.map(&:first)
+    array.last
+
+  end
+
+end
+
+
